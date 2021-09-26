faa785dcf8103e49aeee80a50a3553e5f5a28bdc
Ryan Wong <ryan@rwmobi.com>
2021-09-26 05:30:53 +0100

    [feature] CivicsApiService: implement API Calls

--- app/src/main/java/com/example/android/politicalpreparedness/network/CivicsApiService.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/network/CivicsApiService.kt
@@ -1,11 +1,16 @@
 package com.example.android.politicalpreparedness.network
 
 import com.example.android.politicalpreparedness.network.jsonadapter.ElectionAdapter
+import com.example.android.politicalpreparedness.network.models.ElectionResponse
+import com.example.android.politicalpreparedness.network.models.RepresentativeResponse
+import com.example.android.politicalpreparedness.network.models.VoterInfoResponse
 import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
 
 private const val BASE_URL = "https://www.googleapis.com/civicinfo/v2/"
 
@@ -27,11 +32,25 @@ private val retrofit = Retrofit.Builder()
  */
 
 interface CivicsApiService {
-    //TODO: Add elections API Call
+    //COMPLETED: Add elections API Call
+    @GET("elections")
+    suspend fun getElections(): ElectionResponse
 
-    //TODO: Add voterinfo API Call
+    //COMPLETED: Add voterinfo API Call
+    @GET("voterinfo")
+    suspend fun getVoterInfo(
+        @Query("electionId")
+        electionId: Int,
+        @Query("address")
+        address: String
+    ): VoterInfoResponse
 
-    //TODO: Add representatives API Call
+    //COMPLETED: Add representatives API Call
+    @GET("representatives")
+    suspend fun getRepresentatives(
+        @Query("address")
+        address: String
+    ): RepresentativeResponse
 }
 
 object CivicsApi {
