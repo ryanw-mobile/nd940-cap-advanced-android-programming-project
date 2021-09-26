794e6b39b228e5da85b8525f48c9902acf8d29ea
Ryan Wong <ryan@rwmobi.com>
2021-09-26 03:53:21 +0100

    [feature] Civic API: Put API key to mykeys.kt and added it to .gitignore

--- app/src/main/java/com/example/android/politicalpreparedness/network/CivicsHttpClient.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/network/CivicsHttpClient.kt
@@ -1,29 +1,30 @@
 package com.example.android.politicalpreparedness.network
 
+import mykeys
 import okhttp3.OkHttpClient
 
-class CivicsHttpClient: OkHttpClient() {
+class CivicsHttpClient : OkHttpClient() {
 
     companion object {
 
-        const val API_KEY = "" //TODO: Place your API Key Here
+        const val API_KEY = mykeys.KEY_CIVICAPI //COMPLETED: Place your API Key Here
 
         fun getClient(): OkHttpClient {
             return Builder()
-                    .addInterceptor { chain ->
-                        val original = chain.request()
-                        val url = original
-                                .url()
-                                .newBuilder()
-                                .addQueryParameter("key", API_KEY)
-                                .build()
-                        val request = original
-                                .newBuilder()
-                                .url(url)
-                                .build()
-                        chain.proceed(request)
-                    }
-                    .build()
+                .addInterceptor { chain ->
+                    val original = chain.request()
+                    val url = original
+                        .url()
+                        .newBuilder()
+                        .addQueryParameter("key", API_KEY)
+                        .build()
+                    val request = original
+                        .newBuilder()
+                        .url(url)
+                        .build()
+                    chain.proceed(request)
+                }
+                .build()
         }
 
     }
