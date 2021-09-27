1c9fb62994bc1e257708473037184af1605a4a4c
Ryan Wong <ryan@rwmobi.com>
2021-09-27 18:22:42 +0100

    [feature] ElectionsViewModel: Implement upcoming elections

--- app/src/main/java/com/example/android/politicalpreparedness/election/ElectionsViewModel.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/election/ElectionsViewModel.kt
@@ -1,16 +1,40 @@
 package com.example.android.politicalpreparedness.election
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.android.politicalpreparedness.database.ElectionDao
+import com.example.android.politicalpreparedness.network.CivicsApi
+import com.example.android.politicalpreparedness.network.models.Election
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 
-//TODO: Construct ViewModel and provide election datasource
+//COMPLETED: Construct ViewModel and provide election datasource
 class ElectionsViewModel(private val dataSource: ElectionDao) : ViewModel() {
 
-    //TODO: Create live data val for upcoming elections
+    //COMPLETED: Create live data val for upcoming elections
+    private val _upcomingElections = MutableLiveData<List<Election>>()
+    val upcoomingElections: LiveData<List<Election>>
+        get() = _upcomingElections
 
     //TODO: Create live data val for saved elections
 
     //TODO: Create val and functions to populate live data for upcoming elections from the API and saved elections from local database
+    private var viewModelJob = Job()
+    private val coroutineScope = CoroutineScope(viewModelJob + Dispatchers.Main)
+    fun fetchUpcomingElections() {
+        coroutineScope.launch {
+            try {
+                val getElectionsDeferred = CivicsApi.retrofitService.getElections()
+                _upcomingElections.value = getElectionsDeferred.elections
+            } catch (e: Exception) {
+                e.printStackTrace()
+                _upcomingElections.value = emptyList()
+            }
+        }
+    }
 
     //TODO: Create functions to navigate to saved or upcoming election voter info
 
