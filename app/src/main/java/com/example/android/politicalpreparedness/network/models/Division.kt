d0c814312fa3a0b4ec884b10a04ec9f08c6b41d6
Ryan Wong <ryan@rwmobi.com>
2021-09-27 21:36:52 +0100

    [chore] Division: update dependency

--- app/src/main/java/com/example/android/politicalpreparedness/network/models/Division.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/network/models/Division.kt
@@ -1,11 +1,11 @@
 package com.example.android.politicalpreparedness.network.models
 
 import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
+import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class Division(
-        val id: String,
-        val country: String,
-        val state: String
+    val id: String,
+    val country: String,
+    val state: String
 ) : Parcelable
