9b77465fa67dfc09ecd71998edd99d289c816994
Ryan Wong <ryan@rwmobi.com>
2021-09-27 21:38:05 +0100

    [feature] ElectionsViewModel: rewrite to use the repository as the data source

--- app/src/main/java/com/example/android/politicalpreparedness/election/ElectionsViewModel.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/election/ElectionsViewModel.kt
@@ -3,48 +3,33 @@ package com.example.android.politicalpreparedness.election
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.example.android.politicalpreparedness.database.ElectionDao
-import com.example.android.politicalpreparedness.network.CivicsApi
 import com.example.android.politicalpreparedness.network.models.Election
+import com.example.android.politicalpreparedness.network.repository.ElectionsRepository
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 
 //COMPLETED: Construct ViewModel and provide election datasource
-class ElectionsViewModel(private val dataSource: ElectionDao) : ViewModel() {
+class ElectionsViewModel(private val repository: ElectionsRepository) : ViewModel() {
 
     //COMPLETED: Create live data val for upcoming elections
-    private val _upcomingElections = MutableLiveData<List<Election>>()
-    val upcoomingElections: LiveData<List<Election>>
-        get() = _upcomingElections
+    val upcoomingElections = repository.upcomingElections
 
     //COMPLETED: Create live data val for saved elections
-    private val _savedElections = MutableLiveData<List<Election>>()
-    val savedElections: LiveData<List<Election>>
-        get() = _savedElections
+    val savedElections = repository.followedElections
 
-    private val _selectedElection = MutableLiveData<Election>()
-    val selectedElection: LiveData<Election>
+    private var _selectedElection = MutableLiveData<Election?>()
+    val selectedElection: LiveData<Election?>
         get() = _selectedElection
 
-    init {
-        _selectedElection.value = null
-        fetchUpcomingElections()
-    }
-
-    //TODO: Create val and functions to populate live data for upcoming elections from the API and saved elections from local database
     private var viewModelJob = Job()
     private val coroutineScope = CoroutineScope(viewModelJob + Dispatchers.Main)
-    fun fetchUpcomingElections() {
+
+    init {
         coroutineScope.launch {
-            try {
-                val getElectionsDeferred = CivicsApi.retrofitService.getElections()
-                _upcomingElections.value = getElectionsDeferred.elections
-            } catch (e: Exception) {
-                e.printStackTrace()
-                _upcomingElections.value = emptyList()
-            }
+            _selectedElection.value = null
+            repository.fetchUpcomingElections()
         }
     }
 
--- app/src/main/java/com/example/android/politicalpreparedness/election/ElectionsViewModelFactory.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/election/ElectionsViewModelFactory.kt
@@ -2,14 +2,15 @@ package com.example.android.politicalpreparedness.election
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import com.example.android.politicalpreparedness.database.ElectionDao
+import com.example.android.politicalpreparedness.network.repository.ElectionsRepository
 
 //COMPLETED: Create Factory to generate ElectionViewModel with provided election datasource
-class ElectionsViewModelFactory(val dataSource: ElectionDao) : ViewModelProvider.Factory {
+class ElectionsViewModelFactory(private val repository: ElectionsRepository) :
+    ViewModelProvider.Factory {
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(ElectionsViewModel::class.java)) {
             @Suppress("UNCHECKED_CAST")
-            return ElectionsViewModel(dataSource) as T
+            return ElectionsViewModel(repository) as T
         }
         throw IllegalArgumentException("Unable to construct ViewModel")
     }
