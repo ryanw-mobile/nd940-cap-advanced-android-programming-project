5b2dc249ad2fa3defbddd8715b4177a49bd0b6fe
Ryan Wong <ryan@rwmobi.com>
2021-11-27 22:24:07 +0000

    [feature] App: introduce back button on the Tool bar

--- app/src/main/java/com/example/android/politicalpreparedness/ui/MainActivity.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/ui/MainActivity.kt
@@ -2,6 +2,10 @@ package com.example.android.politicalpreparedness.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.example.android.politicalpreparedness.R
 import com.example.android.politicalpreparedness.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -13,6 +17,11 @@ class MainActivity : AppCompatActivity() {
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setSupportActionBar(binding.topAppbar)
+
+        // We are using a fixed App name heading, but wish to have the back button
+        val navController = findNavController(R.id.nav_host)
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        binding.topAppbar.setupWithNavController(navController, appBarConfiguration)
         supportActionBar?.setDisplayShowTitleEnabled(false)
     }
 }
