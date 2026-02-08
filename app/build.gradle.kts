24e6bfdd88d8deb5fbc98f74521b5461315c2316
Ryan Wong <ryan@rwmobi.com>
2026-02-08 00:28:21 +0000

    [chore] App: elevate to JDK 21

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -74,8 +74,8 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
 
     // Api key: try environment variable first, then keystore properties file
@@ -119,7 +119,7 @@ kotlin {
         optIn.add("kotlin.time.ExperimentalTime")
         freeCompilerArgs.add("-XXLanguage:+PropertyParamAnnotationDefaultTargetMode")
     }
-    jvmToolchain(17)
+    jvmToolchain(21)
 }
 
 dependencies {
